1817323e608a1d0ba180e17bb4e286acaa6f2eb0
Omar Tamer Ayoub <166947576+EngOayoubFo22@users.noreply.github.com>
2026-02-22 23:17:43 +0200

    Add files via upload

---
+++ Lab1/MCAL/GPIO/gpio-priv.h
@@ -0,0 +1,22 @@
+// #define Reg_name Address
+
+#ifndef GPIO_PRIVATE_H
+#define GPIO_PRIVATE_H
+// ramadan kareem
+
+#define TRISA (*((volatile unsigned char*)0x85))
+#define PORTA (*((volatile unsigned char*)0x05))
+
+
+
+
+
+
+
+
+
+
+
+
+
+ #endif
---
+++ Lab1/MCAL/GPIO/gpio.c
@@ -0,0 +1,1 @@
+#include "gpio-interface.h"
